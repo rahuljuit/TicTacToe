9615d9d280f131b912af4af1bdb86c3f16e16863
rahuljuit <denre4@gmail.com>
2020-09-19 17:00:51 +0530

    Declared the winner

--- src/Components/SquareComponent.js
+++ src/Components/SquareComponent.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Square extends Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        )
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+
 }
 
 export default Square;
